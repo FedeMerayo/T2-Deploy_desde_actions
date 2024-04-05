e68b17ff970918e2a7e649847df8071619ec7f49
FedeMerayo <fmmerayo@gmail.com>
2024-04-04 23:45:09 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "aws" {
     }
 
 resource "aws_security_group" "instance_security_group" {
-name = "instance_security_group6"
+name = "instance_security_group7"
 description = "Security group for EC2 instance"
     
 ingress {
